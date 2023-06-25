937bc6ec9210b796ce1e337ca76c16f88edc686d
root <admin@example.com>
2023-06-25 20:55:26 +0430

    clean code applied a bit

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,49 @@ import PageNotFoundView from '../components/PageNotFoundView.vue'
 import HomeView from '../components/HomeView.vue'
 import InitialTestResult from '../components/InitialTestResult.vue'
 
-const routes =  [
-    { path: '/', name: 'Hello world' ,  component: HelloWorld , meta:{auth:false,test:false}},
-    { path: '/login', name: 'Login' , component: LoginView ,meta:{auth:false,test:false} },
-    { path: '/initialtestexp', name: 'initial test explananation', component: InitialTestExpView, meta:{auth:true,test:false} },
-    { path: '/initialtest' , name: 'initial test', component: InitialTestQView , meta:{auth:true,test:false} } ,
-    { path: '/initialtestresult' , name: 'initial test result', component: InitialTestResult , meta:{auth:true, test:true}} ,
-    { path: '/home' , name: 'home page', component: HomeView , meta:{auth:true,test:true} } ,
-    { path: '/:pathMatch(.*)*', name: '404 Page Not Found' , component: PageNotFoundView},
-  ]
+const routes = [
+  {
+    path: "/",
+    name: "Hello world",
+    component: HelloWorld,
+    meta: { auth: false, test: false },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginView,
+    meta: { auth: false, test: false },
+  },
+  {
+    path: "/initialtestexp",
+    name: "initial test explananation",
+    component: InitialTestExpView,
+    meta: { auth: true, test: false },
+  },
+  {
+    path: "/initialtest",
+    name: "initial test",
+    component: InitialTestQView,
+    meta: { auth: true, test: false },
+  },
+  {
+    path: "/initialtestresult",
+    name: "initial test result",
+    component: InitialTestResult,
+    meta: { auth: true, test: true },
+  },
+  {
+    path: "/home",
+    name: "home page",
+    component: HomeView,
+    meta: { auth: true, test: true },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "404 Page Not Found",
+    component: PageNotFoundView,
+  },
+];
 
 const router = createRouter({
     history: createWebHistory(),
