4044eaa218f542315276f246c22cc6bc36f6cc9b
root <admin@example.com>
2023-06-25 20:52:52 +0430

    navigation guard ok

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,12 @@ import HomeView from '../components/HomeView.vue'
 import InitialTestResult from '../components/InitialTestResult.vue'
 
 const routes =  [
-    { path: '/', name: 'Hello world' ,  component: HelloWorld },
-    { path: '/login', name: 'Login' , component: LoginView},
-    { path: '/initialtestexp', name: 'initial test explananation', component: InitialTestExpView},
-    { path: '/initialtest' , name: 'initial test', component: InitialTestQView} ,
-    { path: '/initialtestresult' , name: 'initial test result', component: InitialTestResult} ,
-    { path: '/home' , name: 'home page', component: HomeView} ,
+    { path: '/', name: 'Hello world' ,  component: HelloWorld , meta:{auth:false,test:false}},
+    { path: '/login', name: 'Login' , component: LoginView ,meta:{auth:false,test:false} },
+    { path: '/initialtestexp', name: 'initial test explananation', component: InitialTestExpView, meta:{auth:true,test:false} },
+    { path: '/initialtest' , name: 'initial test', component: InitialTestQView , meta:{auth:true,test:false} } ,
+    { path: '/initialtestresult' , name: 'initial test result', component: InitialTestResult , meta:{auth:true, test:true}} ,
+    { path: '/home' , name: 'home page', component: HomeView , meta:{auth:true,test:true} } ,
     { path: '/:pathMatch(.*)*', name: '404 Page Not Found' , component: PageNotFoundView},
   ]
 
@@ -24,6 +24,16 @@ const router = createRouter({
     routes,
 });
 
-
+router.beforeEach((to) => {
+  if (to.meta.auth && localStorage.getItem('token') == undefined) {
+    router.push('/login');
+  } 
+  if (to.meta.test && JSON.parse(localStorage.getItem('initial_test')) == false) {
+    router.push('/initialtestexp');
+  }
+  if (!to.meta.test && JSON.parse(localStorage.getItem('initial_test')) == true ) {
+    router.push('/home');
+  }
+})
 
 export default router;
